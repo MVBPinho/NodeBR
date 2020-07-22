a81ce9768bd7b9bcd4b13daa5ab533583c545905
MVBPinho <marcos.vb.pinho@live.com>
2020-07-22 16:42:15 -0300

    Aula de manipulação de arquivos, read and create

---
+++ 06-cli/database.js
@@ -0,0 +1,85 @@
+const {
+    readFile,
+    writeFile
+} = require('fs')
+
+const {
+    promisify
+} = require('util')
+
+const readFileAsync = promisify(readFile)
+const writeFileAsync = promisify(writeFile)
+
+// outra forma de obter dados do json
+// const dadosJson = require('./herois.json')
+
+class Database {
+    constructor() {
+        this.NOME_ARQUIVO = 'herois.json'
+    }
+    async obterDadosArquivo(){
+        const arquivo = await readFileAsync(this.NOME_ARQUIVO, 'utf8')
+        return JSON.parse(arquivo.toString())
+    }
+    async escreverArquivo(dados){
+        await writeFileAsync(this.NOME_ARQUIVO, JSON.stringify(dados))
+        return true
+    }
+    async cadastrar(heroi) {
+        const dados = await this.obterDadosArquivo()
+        const id = heroi.id <= 2 ? heroi.id : Date.now() 
+        /**
+         *  {
+         *  nome: Flash,
+         * poder: Velocidade
+         * }
+         * 
+         * {
+         * id : 123871327
+         * }
+         * 
+         * {
+         * nome: Flash,
+         * poder: Velocidade,
+         * id: 1
+         * }
+        **/
+       const heroidComId = {
+           id, 
+           ...heroi
+       }
+       const dadosFinal = [
+           ...dados, 
+           heroidComId
+       ]
+       /** [
+        * {
+        * nome: Flash
+        * }
+        * ]
+        * 
+        * {
+        * nome: Batman
+        * }
+        * 
+        * [
+        * {
+        *   nome: Flash
+        * },
+        * {
+        *   nome: Batman
+        * }
+        * ]
+       */
+
+        const resultado = await this.escreverArquivo(dadosFinal)
+        return resultado
+    }
+    async listar(id){
+        const dados = await this.obterDadosArquivo()
+        const dadosFiltrados = dados.filter(item => id ? (item.id === id) : true)
+        return dadosFiltrados
+    }
+}
+
+module.exports = new Database()
