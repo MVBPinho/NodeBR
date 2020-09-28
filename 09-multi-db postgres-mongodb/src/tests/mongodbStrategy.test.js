b9426bb588c6017d40786fa6e8ef0878c9fc7046
MVBPinho <marcos.vb.pinho@live.com>
2020-09-28 20:35:42 -0300

    criando metodo create

--- 09-multi-db postgres-mongodb/src/tests/mongodbStrategy.test.js
+++ 09-multi-db postgres-mongodb/src/tests/mongodbStrategy.test.js
@@ -2,6 +2,10 @@ const assert = require('assert')
 const MongoDb = require('./../db/strategies/mongodb')
 const Context = require('./../db/strategies/base/contextStrategy')
 
+const MOCK_HEROI_CADASTRAR = {
+    nome: 'Mulher Maravilha',
+    poder: 'Laço'
+}
 const context = new Context(new MongoDb())
 describe('MongoDB Suite de testes', function () {
     this.beforeAll(async () => {
@@ -13,5 +17,10 @@ describe('MongoDB Suite de testes', function () {
         const expected = 'Conectado'
 
         assert.deepStrictEqual(result, expected)
+    })
+    it.only('cadastrar', async () => {
+        const { nome, poder} = await context.create(MOCK_HEROI_CADASTRAR)
+        assert.deepStrictEqual({nome,poder}, MOCK_HEROI_CADASTRAR)
+
     })
 })
