3deba5cc4fcf75ed9451455b2c32ba46097b5879
MVBPinho <marcos.vb.pinho@live.com>
2020-10-03 01:51:34 -0300

    Listando Herois - GET

---
+++ 14-hapi-listar/src/routes/heroRoutes.js
@@ -0,0 +1,44 @@
+//Sempre que eu for utilizar o extends no JS eu tenho que chamar a classe pai
+// antes de qualquer coisa para depois setar
+
+const BaseRoute = require('./base/baseRoute')
+
+class HeroRoutes extends BaseRoute {
+    constructor(db) {
+        super()
+        this.db = db
+    }
+
+    list() {
+        return {
+            path: '/herois',
+            method: 'GET',
+            handler: (request, headers) => {
+                try {
+                    const {
+                        skip,
+                        limit,
+                        nome } = request.query
+                    let query = {}
+                    if (nome) {
+                        query.nome = nome
+                    }
+
+                    //if( typeof skip === "string")
+                    if (isNaN(skip))
+                        throw Error('O tipo do skip é incorreto')
+
+                    if (isNaN(limit))
+                        throw Error('O tipo do limit é incorreto')
+
+                    return this.db.read(query, parseInt(skip), parseInt(limit))
+                } catch (error) {
+                    console.log('DEU RUIM', error)
+                    return "Erro interno no servidor"
+                }
+            }
+        }
+    }
+}
+
+module.exports = HeroRoutes
